762133ab586b5010ed0cdb7f4dc805f18ae645a2
Lin-ke <1720587660@qq.com>
2023-11-17 11:15:18 +0800

    p

--- engine/source/runtime/function/display/displayer.h
+++ engine/source/runtime/function/display/displayer.h
@@ -1,8 +1,19 @@
 #pragma once
 #ifndef __DISPLAYER_H__
 #define __DISPLAYER_H__
-
+// control the windows
+// send event to the application.
 class Displayer {
+private:
+	static Displayer* p_singleton;
+public:
+	Displayer() {
+		p_singleton = this;
+	}
+	~Displayer() {
+		p_singleton = nullptr;
+	}
 
 };
+
 #endif // !__DISPLAYER_H__
